a4d8fdbafecdf519089250ed9b3b58be8c1b6bf1
matbgn <matrash@bqn.ch>
2021-09-10 16:42:03 +0200

    Update value to be tested + show response on ODS file

--- nummer_rheie.py
+++ nummer_rheie.py
@@ -1,12 +1,15 @@
 import random
 MAX_LIST = 72
 MAX_NUM = 20
+FACTOR_MAX_VAL = 12
 
-dict_reihe = list(range(MAX_NUM)) # [7]
+ONLY_MAX = True
+
+dict_reihe = [MAX_NUM] if ONLY_MAX else list(range(MAX_NUM))
 
 for i in range(round(MAX_LIST/2)):
     iTempVal = random.choice(dict_reihe)
-    iTmpFactor = random.randrange(1,MAX_NUM)
+    iTmpFactor = random.randrange(1,FACTOR_MAX_VAL)
     print(iTmpFactor, "*", iTempVal, "=") #,iValTmp*iTmp)
-    iTmpDivision = random.choice(dict_reihe) * random.randrange(1,MAX_NUM)
+    iTmpDivision = random.choice(dict_reihe) * random.randrange(1,FACTOR_MAX_VAL)
     print(iTmpDivision, "/", iTempVal, "=") #,(iTmp - (iTmp % iValTmp))/iTmp)
